94c9089c7a4cd850eb3832c723038d17636a227e
Iacopo Rozzo <139529350+iacopo-rozzo@users.noreply.github.com>
2023-07-20 11:27:40 +0800

    Update init-te.sh

--- init-te.sh
+++ init-te.sh
@@ -4,7 +4,7 @@ TEMP_PATH=$(mktemp -d)
 echo ${TEMP_PATH}
 version=${1:-""}
 echo $version
-wget https://raw.githubusercontent.com/ir-registry/te/main/task-${version}.tar.gz -P ${TEMP_PATH}
+wget https://raw.githubusercontent.com/ir-registry/te/main/package/task-${version}.tar.gz -P ${TEMP_PATH}
 tar -xvf ${TEMP_PATH}/task-${version}.tar.gz -C ${TEMP_PATH}
 
 cd ${TEMP_PATH}
